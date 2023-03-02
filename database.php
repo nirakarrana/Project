68ed55f9b3fb75b08e2e641fbffbeb051f638b18
Nirakar Bikram Rana <117760662+nirakarrana@users.noreply.github.com>
2023-03-02 20:43:07 +0545

    project
    project

---
+++ database.php
@@ -0,0 +1,29 @@
+<?php
+
+$conn = mysqli_connect('sql213.epizy.com','epiz_33697182','Uxw33HSojMa2umF','epiz_33697182_register');
+if(!$conn){
+    die(mysqli_error($conn));
+}
+
+?><?php
+
+$conn = mysqli_connect('sql213.epizy.com','epiz_33697182','Uxw33HSojMa2umF','epiz_33697182_register');
+if(!$conn){
+    die(mysqli_error($conn));
+}
+
+?><?php
+
+$conn = mysqli_connect('sql213.epizy.com','epiz_33697182','Uxw33HSojMa2umF','epiz_33697182_register');
+if(!$conn){
+    die(mysqli_error($conn));
+}
+
+?><?php
+
+$conn = mysqli_connect('sql213.epizy.com','epiz_33697182','Uxw33HSojMa2umF','epiz_33697182_register');
+if(!$conn){
+    die(mysqli_error($conn));
+}
+
+?>
